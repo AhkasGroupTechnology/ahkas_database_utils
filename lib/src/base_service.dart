64140a0b812a718d23294afb49ac375ccc5548b3
NHOR SOPHEAK <mr.nhorsopheak@gmail.com>
2023-11-25 15:34:11 +0700

    refactor

--- lib/src/base_service.dart
+++ lib/src/base_service.dart
@@ -2,11 +2,11 @@ part of ahkas_database_utils;
 
 abstract class BaseService<T> {
   Future<void> save(T data);
-  Future<void> saveAndSync(T data);
+  Future<void> saveAndSync(T data, Function onSynced);
   Future<bool> update(int id, T data);
-  Future<bool> updateAndSync(int id, T data);
+  Future<bool> updateAndSync(int id, T data, Function onSynced);
   Future<bool> delete(int id);
-  Future<bool> deleteAndSync(int id);
+  Future<bool> deleteAndSync(int id, Function onSynced);
   Future<List<T>> retriveData();
   Future<void> syncData();
 }
