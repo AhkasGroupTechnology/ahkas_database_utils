2481a51ff642aabf35e85e658cdc1f171b0681e5
NHOR SOPHEAK <mr.nhorsopheak@gmail.com>
2023-11-25 14:42:15 +0700

    update

---
+++ lib/src/base_sync_repo.dart
@@ -0,0 +1,10 @@
+abstract class NetworkRepoInterface<T> {
+  Future<int> addData(T data) => Future.value(0);
+  Future<bool> saveDataBatch(List<T> data) => Future.value(false);
+  Future<bool> updateData(T data) => Future.value(false);
+  Future<List<T>> getAllData() => Future.value([]);
+}
+
+abstract class LocalRepoInterface<T> extends NetworkRepoInterface<T> {
+  Future<List<T>> getAllUnSyncData();
+}
